e8e5b9526390fab39e0640a52f642b4d27a40a94
Adam <malandro624@gmail.com>
2023-06-08 05:01:40 +0300

    added database connection and added register user functionality

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require("express");
+const dbconnect = require("./config/dbconnect");
 const app = express();
 const dotnev = require("dotenv").config();
-const port = process.env.PORT || 5000;
+const authRouter = require("./routes/authRoutes");
+const bodyParser = require("body-parser");
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+const port = process.env.PORT || 4000;
+
+dbconnect();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/api/user", authRouter);
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+	console.log(`Server running on port ${port}`);
 });
---
+++ models/userModel.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose"); // Erase if already required
+
+// Declare the Schema of the Mongo model
+var userSchema = new mongoose.Schema({
+	first_name: {
+		type: String,
+		required: true,
+	},
+	last_name: {
+		type: String,
+		required: true,
+	},
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	mobile: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+});
+
+//Export the model
+module.exports = mongoose.model("User", userSchema);
